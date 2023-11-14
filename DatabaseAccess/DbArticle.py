bbaf803898d29942ab9387416d5f750e3c274795
KennethFlye <10434642@ucn.dk>
2023-11-14 12:49:10 +0100

    Insert article connection in a with statement

--- DatabaseAccess/DbArticle.py
+++ DatabaseAccess/DbArticle.py
@@ -28,24 +28,31 @@ class DbArticle:
         self.connection = pgres.connect(host=config("HOST"), dbname=config("DATABASE"), user=config("USER"), password=config("PASSWORD"))
 
     def insert_article(self, article):
-        connection = pgres.connect(host=config("HOST"), dbname=config("DATABASE"), user=config("USER"), password=config("PASSWORD"))
-        cursor = connection.cursor()
-        
-        cursor.execute(self.InsertArticleQuery, {'submitter': article.submitter, 'submitter_is_preferred': article.submitter_is_preferred, 'authors_is_preferred': article.authors_is_preferred, 'title': article.title, 'title_is_preferred': article.title_is_preferred, 'comments_is_preferred': article.comments_is_preferred, 'journal_ref': article.journal_ref, 'journal_ref_is_preferred': article.journal_ref_is_preferred, 'doi': article.doi, 'doi_is_preferred': article.doi_is_preferred, 'report_number': article.report_number, 'report_number_is_preferred': article.report_number_is_preferred, 'categories_is_preferred': article.categories_is_preferred, 'license': article.license, 'license_is_preferred': article.license_is_preferred, 'abstract': article.abstract, 'abstract_is_preferred': article.abstract_is_preferred, 'versions': article.version, 'versions_is_preferred': article.versions_is_preferred, 'update_date': article.update_date, 'update_date_is_preferred': article.update_date_is_preferred, 'group': article.group})
-        return_id = cursor.fetchone()
-        article.id = return_id[0]
+        with pgres.connect(host=config("HOST"), dbname=config("DATABASE"), user=config("USER"), password=config("PASSWORD")) as connection:
+            cursor = connection.cursor()
 
-        #Authors
-        self.insert_authors(cursor, article.authors, article.id)
-        #Comments
-        self.insert_comments(cursor, article.comments, article.id)
-        #Categories
-        self.insert_categories(cursor, article.categories, article.id)
+            cursor.execute(self.InsertArticleQuery,
+                           {'submitter': article.submitter, 'submitter_is_preferred': article.submitter_is_preferred,
+                            'authors_is_preferred': article.authors_is_preferred, 'title': article.title,
+                            'title_is_preferred': article.title_is_preferred,
+                            'comments_is_preferred': article.comments_is_preferred, 'journal_ref': article.journal_ref,
+                            'journal_ref_is_preferred': article.journal_ref_is_preferred, 'doi': article.doi,
+                            'doi_is_preferred': article.doi_is_preferred, 'report_number': article.report_number,
+                            'report_number_is_preferred': article.report_number_is_preferred,
+                            'categories_is_preferred': article.categories_is_preferred, 'license': article.license,
+                            'license_is_preferred': article.license_is_preferred, 'abstract': article.abstract,
+                            'abstract_is_preferred': article.abstract_is_preferred, 'versions': article.version,
+                            'versions_is_preferred': article.versions_is_preferred, 'update_date': article.update_date,
+                            'update_date_is_preferred': article.update_date_is_preferred, 'group': article.group})
+            return_id = cursor.fetchone()
+            article.id = return_id[0]
 
-        #Catch Exception? For rollback eller sket det automatisk?
-
-        #connection.rollback()
-        connection.commit()
+            # Authors
+            self.insert_authors(cursor, article.authors, article.id)
+            # Comments
+            self.insert_comments(cursor, article.comments, article.id)
+            # Categories
+            self.insert_categories(cursor, article.categories, article.id)
 
     def get_article_by_id(self, article_id):
         cursor = self.connection.cursor()
@@ -136,7 +143,7 @@ class DbArticle:
         foundArticle.license = article[14]
         foundArticle.license_is_preferred = article[15]
         foundArticle.abstract = article[16]
-        foundArticle.abstracts_is_preferred = article[17]
+        foundArticle.abstract_is_preferred = article[17]
         foundArticle.versions = article[18]
         foundArticle.versions_is_preferred = article[19]
         foundArticle.update_date = article[20]
